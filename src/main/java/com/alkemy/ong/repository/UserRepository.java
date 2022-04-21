0572ab7363d1a6ddcb6b9b12d57933b7983e8a8a
Andres Daniele <andresmdaniele@gmail.com>
2022-04-21 19:52:00 -0300

    fix: Sintax error Soft delete query in UserRepository fixed

--- src/main/java/com/alkemy/ong/repository/UserRepository.java
+++ src/main/java/com/alkemy/ong/repository/UserRepository.java
@@ -9,7 +9,7 @@ import org.springframework.data.repository.query.Param;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
-    @Query("UPDATE users u SET u.active = 0 WHERE u.id = :id")
+    @Query("UPDATE User u SET u.active = 0 WHERE u.id = :id")
     @Modifying
     void softDelete(@Param("id") Integer id);
 
