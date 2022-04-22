dac36f7fc4d93f7d61ffd7af8dda818bfc68bd98
Fede Buchet <fedebuchet17@gmail.com>
2022-04-22 13:40:16 -0300

    agregando el metodo Put para actualizar categorias

--- src/main/java/com/alkemy/ong/controller/CategoryController.java
+++ src/main/java/com/alkemy/ong/controller/CategoryController.java
@@ -2,6 +2,7 @@ package com.alkemy.ong.controller;
 
 import com.alkemy.ong.dto.CategoryDto;
 import com.alkemy.ong.entity.Category;
+import com.alkemy.ong.mapper.CategoryMapper;
 import com.alkemy.ong.service.CategoryService;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,7 +12,6 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.stream.Stream;
 
 import static com.alkemy.ong.utility.Constantes.CATEGORY_URL;
 
@@ -22,13 +22,14 @@ public class CategoryController {
 
     @Autowired
     private CategoryService categoryService;
+    @Autowired
+    private CategoryMapper categoryMapper;
 
-    @PostMapping(value = "/{id}")
+    @PutMapping(value = "/{id}")
     public ResponseEntity<?> update(@PathVariable(value = "id") Long id, @RequestBody Category category) {
         Map<String, Object> response = new HashMap<>();
-        Category category1 = Stream.of(categoryService.findById(id)).map(category2 ->
-                new Category(category2.getId(), category2.getName(), category2.getDescription(),
-                        category2.getImage())).findFirst().orElse(null);
+
+        Category category1 = categoryMapper.categoryDtoToCategory(categoryService.findById(id));
 
         if (category1 == null) {
             response.put("error", "No se encontro ela categoria a actualizar");
@@ -36,10 +37,10 @@ public class CategoryController {
         }
 
         category1 = category;
-        CategoryDto categoryDto=categoryService.save(Stream.of(category1).map(cat->new CategoryDto(cat.getId(),cat.getName(),cat.getDescription(),cat.getImage())).findFirst().get());
-        response.put("category",categoryDto);
-        response.put("mensaje","La categoria ha sido actualizada con exito");
-        return new ResponseEntity<>(response,HttpStatus.OK);
+        CategoryDto categoryDto=categoryService.save(categoryMapper.categoryToCategoryDto(category1));
+        response.put("category", categoryDto);
+        response.put("mensaje", "La categoria ha sido actualizada con exito");
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
-    
+
 }
