a331518cc0a46ee2747cd52b88aa1e4b91fd05a7
JuanCruzD98 <juancdurand@hotmail.com>
2022-04-27 18:16:34 -0300

    minor fixes

--- src/main/java/com/alkemy/ong/repository/NewsRepository.java
+++ src/main/java/com/alkemy/ong/repository/NewsRepository.java
@@ -1,14 +1,10 @@
 package com.alkemy.ong.repository;
 
-import com.alkemy.ong.entity.News;
-import org.hibernate.annotations.SQLDelete;
-import org.hibernate.annotations.Where;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface NewsRepository extends JpaRepository<News, Long> {
