710e59d8f21186a3ae0aa95e04935ab309c906e1
Daniel Chaulet <hana23@gmail.com>
2022-05-02 15:53:05 -0300

    Slides entity

---
+++ src/main/java/com/alkemy/ong/entity/Slide.java
@@ -0,0 +1,45 @@
+package com.alkemy.ong.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "slides")
+public class Slide {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private String imageUrl;
+
+    @NotNull
+    @Column(columnDefinition = "TEXT")
+    private String text;
+
+    @NotNull
+    private Integer order;
+
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "organization_id")
+    private Organization organization;
+
+
+    public Slide(String imageUrl, String text, Integer order, Organization organization)
+    {
+        this.imageUrl = imageUrl;
+        this.text = text;
+        this.order = order;
+        this.organization = organization;
+    }
+}
