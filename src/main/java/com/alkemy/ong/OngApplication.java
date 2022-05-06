ce4c1d36afa47b408768fd7ab608cb6601b02348
Andres Daniele <andresmdaniele@gmail.com>
2022-05-06 18:34:43 -0300

    fix: Security auto configuration class exclude in main class

--- src/main/java/com/alkemy/ong/OngApplication.java
+++ src/main/java/com/alkemy/ong/OngApplication.java
@@ -7,11 +7,12 @@ import com.fasterxml.jackson.databind.SerializationFeature;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 
-@SpringBootApplication(scanBasePackages = {"test"} , exclude = JpaRepositoriesAutoConfiguration.class)
+@SpringBootApplication(scanBasePackages = {"test"} , exclude = {JpaRepositoriesAutoConfiguration.class, SecurityAutoConfiguration.class})
 @EnableTransactionManagement
 
 public class OngApplication {
