dfa21cb5ce8067bf9491086e46f628a28352c12a
Andres Daniele <andresmdaniele@gmail.com>
2022-05-09 11:07:20 -0300

    fix: Unnecesary objectMapper removed from main class

--- src/main/java/com/alkemy/ong/OngApplication.java
+++ src/main/java/com/alkemy/ong/OngApplication.java
@@ -12,7 +12,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 
-@SpringBootApplication//(scanBasePackages = {"test"}, exclude = {JpaRepositoriesAutoConfiguration.class, SecurityAutoConfiguration.class})
+@SpringBootApplication
 @EnableTransactionManagement
 
 public class OngApplication {
@@ -20,15 +20,5 @@ public class OngApplication {
     public static void main(String[] args) {
         SpringApplication.run(OngApplication.class, args);
     }
-
-    @Bean
-    public ObjectMapper objectMapper() {
-        return new ObjectMapper()
-                .setDefaultPropertyInclusion(JsonInclude.Include.NON_NULL)
-                .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
-                .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
-                .findAndRegisterModules();
-
-    }
-
+    
 }
