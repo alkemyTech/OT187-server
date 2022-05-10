aea85ae11f246ce84b334f0c7332196367cb6a2c
Andres Daniele <andresmdaniele@gmail.com>
2022-05-09 21:31:08 -0300

    feat: Member service and its implementatios method for pagination added

--- src/main/java/com/alkemy/ong/service/MemberServiceImpl.java
+++ src/main/java/com/alkemy/ong/service/MemberServiceImpl.java
@@ -1,35 +1,64 @@
 package com.alkemy.ong.service;
 
+import com.alkemy.ong.dto.MemberDto;
+import com.alkemy.ong.dto.PageResponseDto;
 import com.alkemy.ong.entity.Member;
-import com.alkemy.ong.repository.MemberRespository;
+import com.alkemy.ong.exception.NotFoundException;
+import com.alkemy.ong.mapper.MemberMapper;
+import com.alkemy.ong.repository.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
+import static com.alkemy.ong.utility.Constantes.PAGE_SIZE;
+import static com.alkemy.ong.utility.Constantes.PAGE_URL;
 
 @Service
 public class MemberServiceImpl implements MemberService {
 
     @Autowired
-    private MemberRespository memberRespository;
+    private MemberRepository memberRepository;
+    @Autowired
+    private MemberMapper memberMapper;
 
     @Override
-    public List<Member> findAll() {
-        return memberRespository.findAll();
+    @Transactional(readOnly = true)
+    public PageResponseDto<MemberDto> getAll(Integer page) {
+
+        if (page == null || page < 1) {
+            throw new NotFoundException("Page must be greater than 0 and less than or equal to the total number of pages");
+        }
+
+        Pageable pageable = PageRequest.of(page - 1, PAGE_SIZE);
+        Page<Member> members = memberRepository.findAll(pageable);
+
+        if (members.isEmpty()) {
+            throw new NotFoundException("The requested page does not exist");
+        }
+
+        Page<MemberDto> memberDto = members.map(memberMapper::memberToMemberDto);
+
+        return new PageResponseDto<>(memberDto, PAGE_URL);
     }
 
     @Override
-    public Member findById(Long id) {
-        return memberRespository.findById(id).get();
+    public MemberDto findById(Long id) {
+        Member member = memberRepository.findById(id).orElseThrow(() -> new NotFoundException("Member not found"));
+
+        return memberMapper.memberToMemberDto(memberRepository.findById(id).get());
+
     }
 
     @Override
-    public Member save(Member member) {
-        return memberRespository.save(member);
+    public MemberDto save(Member member) {
+        return memberMapper.memberToMemberDto(memberRepository.save(member));
     }
 
     @Override
     public void deleteById(Long id) {
-        memberRespository.deleteById(id);
+        memberRepository.deleteById(id);
     }
 }
