bb25934ad0acd8a8396ffa4f45e90804f0ab1998
Javi Cáceres Giménez <javi.caceresgimenez@gmail.com>
2022-05-10 12:52:42 -0300

    fix: Deleted a wrong line of code

--- src/main/java/com/alkemy/ong/controller/CommentController.java
+++ src/main/java/com/alkemy/ong/controller/CommentController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Collections;
 import java.util.List;
 
 import static com.alkemy.ong.utility.Constantes.COMMENT_URL;
