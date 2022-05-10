de822dd8bf429d68be2c2d23fc29a7d7b6c45d7a
Javi Cáceres Giménez <javi.caceresgimenez@gmail.com>
2022-05-10 12:54:13 -0300

    feat: added creation date attribute to the comment entity

--- src/main/java/com/alkemy/ong/entity/Comment.java
+++ src/main/java/com/alkemy/ong/entity/Comment.java
@@ -1,10 +1,12 @@
 package com.alkemy.ong.entity;
 
 import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
 
 @Entity
 @Data
@@ -25,5 +27,9 @@ public class Comment {
     
     @NotBlank(message = "It is not possible to send an empty comment")
     private String body;
+    
+    @Column(name = "creation_date", nullable = false)
+    @CreationTimestamp
+    private LocalDateTime creationDate;
     
 }
