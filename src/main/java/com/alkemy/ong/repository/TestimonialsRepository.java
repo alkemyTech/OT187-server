cca96f06d6e21a7cbc6e24c1e27447f3dba11cab
Andres Daniele <andresmdaniele@gmail.com>
2022-05-09 22:27:05 -0300

    feat: JPA method to obtain paginated testimonias from BD added to testimonial respository

--- src/main/java/com/alkemy/ong/repository/TestimonialsRepository.java
+++ src/main/java/com/alkemy/ong/repository/TestimonialsRepository.java
@@ -1,6 +1,8 @@
 package com.alkemy.ong.repository;
-
+;
 import com.alkemy.ong.entity.Testimonial;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -12,4 +14,7 @@ public interface TestimonialsRepository extends JpaRepository<Testimonial, Long>
     @Query("UPDATE Testimonial t SET t.active = 0 WHERE t.id = :id")
     @Modifying
     void softDelete(@Param("id") Long id);
+
+    @Query("SELECT t FROM Testimonial t WHERE t.active = 1")
+    Page<Testimonial> findAll(Pageable pageable);
 }
