5a009c425a890d94569e1ad7f170a8414aaec50e
Andres Daniele <andresmdaniele@gmail.com>
2022-05-11 17:19:58 -0300

    feat: Swagger documentation added to OrganizationController

--- src/main/java/com/alkemy/ong/controller/OrganizationController.java
+++ src/main/java/com/alkemy/ong/controller/OrganizationController.java
@@ -1,9 +1,15 @@
 package com.alkemy.ong.controller;
 
+import com.alkemy.ong.dto.ActivityDto;
 import com.alkemy.ong.dto.OrganizationDto;
 import com.alkemy.ong.dto.OrganizationSlimDto;
 import com.alkemy.ong.mapper.OrganizationMapper;
 import com.alkemy.ong.service.OrganizationServiceImpl;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -26,17 +32,40 @@ public class OrganizationController {
     @Autowired
     private  OrganizationServiceImpl organizationService;
 
-
+    @Operation(summary = "Get all organizations")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful operation. Return a list of organizations",
+                    content = {
+                            @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = OrganizationSlimDto.class))}),
+            @ApiResponse(responseCode = "400", description = "Bad request",
+                    content = @Content)})
     @GetMapping()
     public ResponseEntity<List<OrganizationSlimDto>> getAllOrganizations(){
         return new ResponseEntity<>(mapStructMapper.organizationsToOrganizationsSlimDto(organizationService.getAllOrganizations()), HttpStatus.OK);
     }
 
+    @Operation(summary = "Get organization by name")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201200", description = "Successful operation. Return a organization",
+                    content = {
+                            @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = OrganizationSlimDto.class))}),
+            @ApiResponse(responseCode = "400", description = "Name not found",
+                    content = @Content)})
     @GetMapping(REQUEST_NAME)
     public ResponseEntity<OrganizationSlimDto> getOrganizationByName(@PathVariable(value = "name") String name){
         return new ResponseEntity<>(mapStructMapper.organizationToOrganizationSlimDto(organizationService.findOrganizationByName(name)),HttpStatus.OK);
     }
 
+    @Operation(summary = "Upadate an organization")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Successful operation. Organization updated",
+                    content = {
+                            @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = ActivityDto.class))}),
+            @ApiResponse(responseCode = "500", description = "Dto is not valid",
+                    content = @Content)})
     @PostMapping(REQUEST_ID)
     public ResponseEntity<OrganizationDto> updateOrganization (@PathVariable Long id, @RequestBody OrganizationDto organizationDto) {
         return ResponseEntity.ok().body(organizationService.updateOrganization(id, organizationDto));
