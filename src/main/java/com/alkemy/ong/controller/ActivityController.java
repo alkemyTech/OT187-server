ab92b6f7da3c40ec132dffc4df5bf187fe9deca3
Andres Daniele <andresmdaniele@gmail.com>
2022-05-11 17:18:46 -0300

    feat: Swagger documentation added to activity controller

--- src/main/java/com/alkemy/ong/controller/ActivityController.java
+++ src/main/java/com/alkemy/ong/controller/ActivityController.java
@@ -2,6 +2,11 @@ package com.alkemy.ong.controller;
 
 import com.alkemy.ong.dto.ActivityDto;
 import com.alkemy.ong.service.ActivityService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,11 +23,27 @@ public class ActivityController {
     @Autowired
     private ActivityService activityService;
 
+    @Operation(summary = "Create a new activity")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Successful operation. Activity created",
+                    content = {
+                            @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = ActivityDto.class))}),
+            @ApiResponse(responseCode = "500", description = "Dto is not valid",
+                    content = @Content)})
     @PostMapping()
     public ResponseEntity<ActivityDto> createActivity(@RequestBody ActivityDto activityDto) {
         return ResponseEntity.status(HttpStatus.CREATED).body(activityService.createActivity(activityDto));
     }
 
+    @Operation(summary = "Update an activity")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful operation. Activity updated",
+                    content = {
+                            @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = ActivityDto.class))}),
+            @ApiResponse(responseCode = "400", description = "Activity not found",
+                    content = @Content)})
     @PutMapping(REQUEST_ID)
     public ResponseEntity<ActivityDto> updateActivity(@PathVariable Long id, @RequestBody ActivityDto activityDto) {
         return ResponseEntity.ok().body(activityService.updateActivity(id, activityDto));
