d3ecfe10ce890e45b663104eaf1585731e9033de
JuanCruzD98 <juancdurand@hotmail.com>
2022-05-12 18:50:16 -0300

    Feat: added roles and users data seed

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,25 +1,26 @@
 INSERT INTO activities(name,content,deleted) VALUES('Ayuda humanitaria','Se brinda ayuda a los mas necesitados',0)
 INSERT INTO activities(name,content,deleted) VALUES('Recaudacion de fondos','Se recaudaran fondos para realizar actividades',0)
 INSERT INTO activities(name,content,deleted) VALUES('Reparto de folletos','Se reparten folletos con informacion sobre nuestra tarea',0)
-
+INSERT INTO roles(name,description,create_at) VALUES ('ADMIN','Administrator','2022/05/04'),
+('USER','Regular user','2022/05/12');
 INSERT INTO users (first_name,last_name,email,password,photo,role_id,creation_date) VALUES
-('Pedro','Perez','emailgenerico1@email.com','contraseña1','http://imagen1.gl','USER','23/09/2022'),
-('Pablo','Martinez','emailgenerico2@email.com','contraseña2','http://imagen2.gl','USER','18/04/2022'),
-('Martin','Gimenez','emailgenerico3@email.com','contraseña3','http://imagen3.gl','USER','15/08/2022'),
-('Juan','Sanchez','emailgenerico4@email.com','contraseña4','http://imagen4.gl','USER','31/05/2022'),
-('Ignacio','Perez','emailgenerico5@email.com','contraseña5','http://imagen5.gl','USER','04/06/2022'),
-('Miguel','Fernandez','emailgenerico6@email.com','contraseña6','http://imagen6.gl','USER','08/07/2022'),
-('Carlos','Hernandez','emailgenerico7@email.com','contraseña7','http://imagen7.gl','USER','17/02/2022'),
-('Lucas','Azambuyo','emailgenerico8@email.com','contraseña8','http://imagen8.gl','USER','13/01/2022'),
-('Ruben','Robles','emailgenerico9@email.com','contraseña9','http://imagen9.gl','USER','11/06/2022'),
-('Joaquin','Martinez','emailgenerico10@email.com','contraseña1','http://imagen10.gl','USER','10/11/2022'),
-('Mario','Sanchez','emailgenerico11@email.com','contraseña11','http://imagen11.gl','ADMIN','18/09/2022'),
-('Raul','Hernandez','emailgenerico12@email.com','contraseña12','http://imagen12.gl','ADMIN','05/07/2022'),
-('Raquel','Ramirez','emailgenerico13@email.com','contraseña13','http://imagen13.gl','ADMIN','13/11/2022'),
-('Micaela','Fernandez','emailgenerico14@email.com','contraseña14','http://imagen14.gl','ADMIN','15/06/2022'),
-('Agustina','Robles','emailgenerico15@email.com','contraseña15','http://imagen15.gl','ADMIN','23/11/2022'),
-('Patricia','Zapata','emailgenerico16@email.com','contraseña16','http://imagen16.gl','ADMIN','09/06/2022'),
-('Eugenia','Fernandez','emailgenerico17@email.com','contraseña17','http://imagen17.gl','ADMIN','08/11/2022'),
-('Joaquin','Ramirez','emailgenerico18@email.com','contraseña18','http://imagen18.gl','ADMIN','23/12/2022'),
-('Carlos','Perez','emailgenerico19@email.com','contraseña19','http://imagen19.gl','ADMIN','13/05/2022'),
-('Martin','Martinez','emailgenerico20@email.com','contraseña20','http://imagen20.gl','ADMIN','09/11/2022');
+('Pedro','Perez','emailgenerico1@email.com','contraseña1','http://imagen1.gl','2','2022/05/12'),
+('Pablo','Martinez','emailgenerico2@email.com','contraseña2','http://imagen2.gl','2','2022/05/12'),
+('Martin','Gimenez','emailgenerico3@email.com','contraseña3','http://imagen3.gl','2','2022/05/12'),
+('Juan','Sanchez','emailgenerico4@email.com','contraseña4','http://imagen4.gl','2','2022/05/12'),
+('Ignacio','Perez','emailgenerico5@email.com','contraseña5','http://imagen5.gl','2','2022/05/12'),
+('Miguel','Fernandez','emailgenerico6@email.com','contraseña6','http://imagen6.gl','2','2022/05/12'),
+('Carlos','Hernandez','emailgenerico7@email.com','contraseña7','http://imagen7.gl','2','2022/05/12'),
+('Lucas','Azambuyo','emailgenerico8@email.com','contraseña8','http://imagen8.gl','2','2022/05/12'),
+('Ruben','Robles','emailgenerico9@email.com','contraseña9','http://imagen9.gl','2','2022/05/12'),
+('Joaquin','Martinez','emailgenerico10@email.com','contraseña1','http://imagen10.gl','2','2022/05/12'),
+('Mario','Sanchez','emailgenerico11@email.com','contraseña11','http://imagen11.gl','1','2022/05/12'),
+('Raul','Hernandez','emailgenerico12@email.com','contraseña12','http://imagen12.gl','1','2022/05/12'),
+('Raquel','Ramirez','emailgenerico13@email.com','contraseña13','http://imagen13.gl','1','2022/05/12'),
+('Micaela','Fernandez','emailgenerico14@email.com','contraseña14','http://imagen14.gl','1','2022/05/12'),
+('Agustina','Robles','emailgenerico15@email.com','contraseña15','http://imagen15.gl','1','2022/05/12'),
+('Patricia','Zapata','emailgenerico16@email.com','contraseña16','http://imagen16.gl','1','2022/05/12'),
+('Eugenia','Fernandez','emailgenerico17@email.com','contraseña17','http://imagen17.gl','1','2022/05/12'),
+('Joaquin','Ramirez','emailgenerico18@email.com','contraseña18','http://imagen18.gl','1','2022/05/12'),
+('Carlos','Perez','emailgenerico19@email.com','contraseña19','http://imagen19.gl','1','2022/05/12'),
+('Martin','Martinez','emailgenerico20@email.com','contraseña20','http://imagen20.gl','1','2022/05/12');
