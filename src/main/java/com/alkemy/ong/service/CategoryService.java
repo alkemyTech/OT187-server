f03e426e2bd52693af1efba49251339c34386f87
FedeVB <93800198+FedeVB@users.noreply.github.com>
2022-05-15 19:10:23 -0300

    Categories fixed (#49)
    * se arreglaron los errores del controlador de categorias y se elimino el controlador erroneo
    
    * definiendo el servicio de categorias
    
    * se agrego la paginacion de categorias y una ruta al metodo Post de categorias

--- src/main/java/com/alkemy/ong/service/CategoryService.java
+++ src/main/java/com/alkemy/ong/service/CategoryService.java
@@ -1,12 +1,18 @@
 package com.alkemy.ong.service;
 
 import com.alkemy.ong.dto.CategoryDto;
+import com.alkemy.ong.entity.Category;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+
+import java.util.List;
 
 public interface CategoryService {
 
+    List<CategoryDto> findAll();
     CategoryDto save(CategoryDto categoryDto);
     void delete(Long id);
     CategoryDto findById(Long id);
     CategoryDto update(Long id, CategoryDto categoryDto);
-
+    Page<Category> findAll(Integer page);
 }
